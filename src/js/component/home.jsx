37f9bc983b7bb86ebdd4dd611bcb5b9d62f8a80e
AugustoSchemberger <130497560+AugustoSchemberger@users.noreply.github.com>
2023-09-08 16:05:53 +0000

    Actualización del proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	
+	const [todoList, setTodoList] = React.useState([{ label: "Hacer la cama", done: false }, { label: "Pasear al perro", done: false }, { label: "Preparar la comida", done: false }, { label: "Limpiar la casa", done: false }, { label: "Hacer la compra", done: false }])
+	const [inputValue, setInputValue] = React.useState("")
+	
+	const handleAddNewTodo = () => {
+		setTodoList(prev => [...prev, { label: inputValue, done: false }])
+	}
+	
+	const handleDeleteTodo = (indexToRemove) => {
+		setTodoList(prev => prev.filter((element, index) => index !== indexToRemove))
+	}
+	
+	const handleOnKeyDown = (e) => {
+		const keyPressed = e.key
+		if (inputValue && keyPressed === "Enter") {
+			handleAddNewTodo()
+			setInputValue("")
+		}
+	}
+	
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>Todo list</h1>
+			<hr />
+			<input onKeyDown={handleOnKeyDown} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+			{
+				todoList.map((singleTodo, index) => {
+					return <ul style={{ margin: "3rem" }}>
+						<section style={{ display: "flex", justifyContent: "space-between" }}>
+							<li key={index}>{singleTodo.label}</li>
+							<button onClick={() => handleDeleteTodo(index)}>{`Eliminar ${singleTodo.label}`}</button>
+						</section>
+					</ul>
+				})
+				//
+			}
 		</div>
 	);
 };
-
 export default Home;
